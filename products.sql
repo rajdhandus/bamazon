60f761f4774923c8c3289998524185fd38d3d394
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2018-06-14 22:15:29 -0400

    Update products.sql

--- products.sql
+++ products.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products(
-item_id INTEGER(10) NOT NULL,
+item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
 price DECIMAL(20,2) NOT NULL,
